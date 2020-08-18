649dc812ab69f19555bdad543436545e75f502d2
gustavo <gustavo26cs@gmail.com>
2020-08-18 16:57:50 -0300

    Added validation to posts and application

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_filter :authentication_user!, except: [:show, :index]
 
   # GET /posts
   # GET /posts.json
